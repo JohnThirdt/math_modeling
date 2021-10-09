b483bb728d173cee0583f603b3e7d0865465b053
JohnThirdt <91802503+JohnThirdt@users.noreply.github.com>
2021-10-09 14:29:32 +0000

    Lec 1 actually done

---
+++ lec_1_list.py
@@ -0,0 +1,29 @@
+a = [1, 3, 6]
+print(a[2])
+
+b = [8, 10, 11]
+
+c = a + b
+print(c)
+
+#c = a - b
+
+print(c * 3)
+
+c.append('Good')
+print(c)
+
+a.append(b)
+c = a
+print(c)
+
+print(len(a))
+
+d = [3, 4, 5]
+w = [3, 6, 8]
+
+d.append(w)
+
+print(d)
+
+print(len(d))
